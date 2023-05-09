b1a11acbe253a8dc713be9fdf117d533b1589ace
magnus.kodehode <magnus.kodehode@gmail.com>
2023-05-09 11:11:41 +0200

    fixed image urls for build

--- src/data/prosjekter.js
+++ src/data/prosjekter.js
@@ -5,7 +5,7 @@ export const prosjekter =
         prosjektNavn: 'Juleprosjekt',
         prosjektUrl: 'https://magnus-kodehode.github.io/juleoppgave/',
         github: 'https://github.com/magnus-kodehode/juleoppgave',
-        prosjektThumbnail: '../../thumbnails/julThumbnail.png',
+        prosjektThumbnail: './thumbnails/julThumbnail.png',
         altTekst: 'En nettside med juletema',
         prosjektBeskrivelse: 'En side som inkluderer nedtelling til jul, og muligheten til å skrive julehilsner som lagres i localstorage.'
     },
@@ -14,7 +14,7 @@ export const prosjekter =
         prosjektNavn: 'Color Picker',
         prosjektUrl: 'https://magnus-kodehode.github.io/colorPicker/',
         github: 'https://github.com/magnus-kodehode/colorPicker',
-        prosjektThumbnail: '../../thumbnails/colorPicker.png',
+        prosjektThumbnail: './thumbnails/colorPicker.png',
         altTekst: 'En nettside for å finne et utvalg av farger',
         prosjektBeskrivelse: 'En side som tar utgangspunkt i en selvvalgt farge, og gir deg et utvalg komplimenterende farger'
     }
@@ -22,11 +22,3 @@ export const prosjekter =
    
 ]
 
-/* 
-        "id": "1",
-        "prosjektNavn": "",
-        "prosjektUrl": "",
-        "prosjektThumbnail": "",
-        "alt-tekst": "",
-        "prosjektBeskrivelse": ""
-*/
